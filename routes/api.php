1523d510d7dbea5c20482865b0737db8286cc7e5
developer-uzzal <mduzzal999111@gmail.com>
2026-05-25 15:15:19 +0600

    initial project

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/search', [ProductController::class, 'search']);
+Route::get('/products/dashboard', [ProductController::class, 'dashboard']);
+Route::get('/products/sales-report', [ProductController::class, 'salesReport']);
+
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/filter', [OrderController::class, 'filterByStatus']);
+
