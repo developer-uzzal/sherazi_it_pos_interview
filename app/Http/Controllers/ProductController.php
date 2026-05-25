7aeff16053f28b902a56a7f2686e0b16d8f764b4
developer-uzzal <mduzzal999111@gmail.com>
2026-05-25 15:25:22 +0600

    initialazation predis

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,6 +24,9 @@ class ProductController extends Controller
             ];
         }
 
+
+        
+
         return response()->json($result);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Redis::set('test', 'Hello Redis');
